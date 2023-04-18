90b037edc80bcc9b861f4e4352f547ea6401f77b
ItsMeRioooo <74697068+ItsMeRioooo@users.noreply.github.com>
2023-04-19 00:50:41 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+const { token } = require('./config.json');
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.login(token);
